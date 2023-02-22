338d5937c66bf4fabc9ebbd33a61e12ec7df4626
XionMingi <mgkim@xionits.com>
2023-02-22 15:48:57 +0900

    webQuery show table 기능 추가

--- common/DB/jdbc.py
+++ common/DB/jdbc.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import pandas as pd
 import psycopg2
 import json
+import unicodedata
 
 with open("setting.json", encoding="UTF-8") as f:
     SETTING = json.loads(f.read())
@@ -269,6 +270,37 @@ def db_alter(qry):
                 error_list.insert(0, error + ']')
             data = {'status': 400, 'data': error_list, 'type': 'create'}
     return data
+def db_show(qry):
+    qry=qry.lower()
+    try :
+        td_context = create_context(host="1.223.168.93:44240", username="dbc", password="dbc", logmech="TD2")
+        result = td_context.execute(qry)
+        data = result.fetchall()
+        data = pd.DataFrame(data)
+        data = data.fillna('NULL')
+        print("===============================")
+        print("Success")
+        print("===============================")
+        data = {'status' : 200, 'data' : data, 'type' : 'select'}
+    except Exception as e :
+        if 'Failed to connect to Teradata Vantage' in str(e) :
+            data = {'status' : 404, 'data' : 'Failed to connect to Teradata Vantage', 'type' : 'select'}
+
+        else :
+            if '[Error' in str(e) :
+                err_index = str(e).find('[Error')
+                err_index = str(e).index(']', err_index)
+                error = str(e)[0 : err_index]
+                error_list = str(e).strip(error).split('at ')
+                for i in range(len(error_list)) :
+                    if i == 0 :
+                        error_list[i] = 'S' + error_list[i]
+                        continue
+                    error_list[i] = 'at ' + error_list[i]
+                error_list.insert(0, error + ']')
+            data = {'status' : 400, 'data' : error_list, 'type' : 'select'}
+
+    return  data
 def connect(data):
     if data['db'] == 'Teradata' :
         try :
